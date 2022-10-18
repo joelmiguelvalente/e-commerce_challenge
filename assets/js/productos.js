e3ae984d4e148c487af38569e689d50031ba2ae7
Miguel92 <joelmiguelvalente@gmail.com>
2022-10-18 16:23:41 -0300

    Challenge-E-Commerce: 0004 BETA - Cambio al insertar las secciones y login agregado en el commit anteriror

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -10,11 +10,7 @@ const secciones = async () => {
 	const banner = document.querySelector(".section__banner")
 	const obj = await response()
 	const categorias = obj.categories
-<<<<<<< HEAD
-	categorias.forEach( categoria => banner.after(template.agregar_section(categoria.nombre, categoria.seo)))
-=======
 	categorias.forEach( categoria => banner.before(template.agregar_section(categoria.nombre, categoria.seo)))
->>>>>>> 84b232e73232829cb97bf384ddcc7aa63b19dd28
 }
 
 const productos = async () => {
@@ -26,10 +22,6 @@ const productos = async () => {
 		lista.forEach( producto => {
 			const divPro = document.querySelector(`.${categoria.seo} .productos__grid`)
 			divPro.append(template.agregar_tarjeta(producto, categoria.seo))
-<<<<<<< HEAD
-=======
-			//divPro.appendChild(template.agregar_section(categoria.nombre, categoria.seo))
->>>>>>> 84b232e73232829cb97bf384ddcc7aa63b19dd28
 		})
 	})
 	
