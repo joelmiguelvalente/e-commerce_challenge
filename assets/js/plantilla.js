84b232e73232829cb97bf384ddcc7aa63b19dd28
Miguel92 <joelmiguelvalente@gmail.com>
2022-10-18 15:53:16 -0300

    Challenge-E-Commerce: 0003 BETA - Crea secciones, agrega productos automaticamente y cambios en db.json

--- assets/js/plantilla.js
+++ assets/js/plantilla.js
@@ -1,12 +1,28 @@
-const createDIV = () => document.createElement("div")
+const create = type => document.createElement(type)
+
+const agregar_section = (nombre, seo) => {
+	const section = create("section")
+	section.classList.add('productos', seo)
+
+	const bloque = `<div class="container">
+		<div class="productos__header d--flex jc--between ai--center">
+			<div class="productos__header--title">${nombre}</div>
+			<a href="${seo}.html" class="productos__header--link d--flex jc--end ai--center">Ver todo <iconify-icon icon="fluent:arrow-right-20-filled"></iconify-icon></a>
+		</div>
+		<div class="productos__grid d--grid gap--3">
+		</div>
+	</div>`
+	section.innerHTML = bloque
+	return section
+}
 
 const agregar_tarjeta = (elemento, pagina) => {
 	// Texto
 	const numero = elemento.title.split('_')[1]
 	const alt = elemento.tags.join(" ")
 	// Creamos el contenedor de la columna
-	const gridColumn = createDIV()
-	gridColumn.classList.add("productos__grid--column")
+	const gridColumn = create("div")
+	gridColumn.classList.add("productos__grid--column", elemento.id)
 	//
 	let bloque = `<div class="productos__card rounded--2 shadow overflow--hidden">
 		<img class="productos__card--img" src="${elemento.image}" alt="${alt}" title="${elemento.title}">
@@ -20,4 +36,4 @@ const agregar_tarjeta = (elemento, pagina) => {
 	return gridColumn
 }
 
-export const template = { agregar_tarjeta }
+export const template = { agregar_tarjeta, agregar_section }
